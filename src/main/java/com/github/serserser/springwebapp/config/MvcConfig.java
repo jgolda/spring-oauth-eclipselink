8801eaed1d12a9effc0c7704b7a160539cc4da8b
Jacek Gołda <strefowy.jacek@gmail.com>
2017-10-16 19:42:19 +0200

    Poprawne rozwiązania serwowania statycznej aplikacji angularowej

--- src/main/java/com/github/serserser/springwebapp/config/MvcConfig.java
+++ src/main/java/com/github/serserser/springwebapp/config/MvcConfig.java
@@ -1,29 +1,22 @@
 package com.github.serserser.springwebapp.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableWebMvc
 public class MvcConfig extends WebMvcConfigurerAdapter {
 
-    @Bean
-    public ViewResolver viewResolver() {
-        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-        resolver.setViewClass(JstlView.class);
-        resolver.setPrefix("/WEB-INF/views/");
-        resolver.setSuffix(".jsp");
-        return resolver;
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
     }
 
     @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/static/**").addResourceLocations("/WEB-INF/views/static/");
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("/static/application.html");
     }
 }
---
+++ src/main/resources/static/loginController.js
@@ -0,0 +1,44 @@
+var module = angular.module('App', ["ngResource", "ngRoute", "ngCookies"]);
+
+module.controller('LoginController',
+    function ($scope, $http, $httpParamSerializer, $cookies) {
+
+        $scope.error;
+
+        $scope.data = {
+            username: '',
+            password: '',
+            grant_type: 'password',
+            client_id: 'clientIdPassword'
+        };
+
+        $scope.encoded = btoa('clientIdPassword:secret');
+
+        $scope.login = function() {
+            console.log('login request for login: ' + $scope.data.username + ', password: ' + $scope.data.password);
+            $scope.error = '';
+            var request = {
+                method: 'POST',
+                url: 'http://localhost:8080/app/oauth/token',
+                headers: {
+                    "Authorization": "Basic " + $scope.encoded,
+                    "Content-type": "application/x-www-form-urlencoded; charset=utf-8"
+                },
+                data: $httpParamSerializer($scope.data)
+            };
+
+            $http(request).then(
+                function (response) {
+                    $http.defaults.headers.Authorization = 'Bearer ' + response.data.access_token;
+                    $cookies.put("access_token", response.data.acces_token);
+                    console.log('received token: ' + response.data.access_token);
+                },
+                function (response) {
+                    $scope.error = response.data.error_description;
+                }
+            );
+            $scope.data.username = '';
+            $scope.data.password = '';
+        }
+    }
+);
